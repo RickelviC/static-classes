0ecd67c9cf5f634c350cb7b117d9a578675dab28
RickelviC <Cruzrickelvi@gmail.com>
2026-05-06 14:48:42 -0400

    needs fixing

--- src/main/java/com/pluralsight/Formatting.java
+++ src/main/java/com/pluralsight/Formatting.java
@@ -1,78 +1,37 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Formatting {
-    private String firstName;
-    private String lastName;
-    private String middleName;
-    private String prefix;
-    private String suffix;
 
-    public Formatting(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.middleName = null;
-        this.prefix = null;
-        this.suffix = null;
+    private Formatting() {
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
+    public static String format_FullName(String firstName, String lastName) {
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1) + " ";;
-    }
+        Scanner scanner = new Scanner(System.in);
 
-    public String getLastName() {
-        return lastName;
-    }
+        while (firstName.isEmpty()) {
+            System.out.println("please enter your first name");
+            System.out.print("First name: ");
+            firstName = scanner.nextLine().trim();
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1);
-
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        if (middleName.isEmpty()) {
-            middleName = "";
-        } else {
-            middleName = middleName.substring(0, 1).toUpperCase() + " ";
+            if (firstName.isEmpty()) {
+                System.out.println("must have a first name");
+            }
         }
-        this.middleName = middleName;
-    }
 
-    public String getPrefix() {
-        return prefix;
-    }
+        while (lastName.isEmpty()) {
+            System.out.println("please enter your Last name");
+            System.out.print("Last name: ");
+            lastName = scanner.nextLine().trim();
 
-    public void setPrefix(String prefix) {
-        if (prefix.isEmpty()) {
-            prefix = "";
-        } else {
-            prefix += " ";
+            if (lastName.isEmpty()) {
+                System.out.println("must have a Last name");
+            }
         }
-        this.prefix = prefix;
-    }
 
-    public String getSuffix() {
-        return suffix;
-    }
-
-    public void setSuffix(String suffix) {
-        if (suffix.isEmpty()) {
-            suffix = "";
-        } else {
-            suffix = ", " + suffix;
-        }
-        this.suffix = suffix;
-    }
-
-    @Override
-    public String toString() {
-        return prefix + firstName +  middleName + lastName + suffix;
+        return lastName + " " + firstName;
     }
 }
+
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -4,49 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String firstName = "";
-        String lastName = "";
-
-        Formatting formatting = new Formatting(firstName, lastName);
-
-        while (firstName.isEmpty()) {
-            System.out.println("please enter your first name");
-            System.out.print("First name: ");
-            firstName = scanner.nextLine().trim();
 
 
-            if (firstName.isEmpty()) {
-                System.out.println("must have a first name");
-            }
-        }
-        formatting.setFirstName(firstName);
-
-        System.out.print("Middle name: ");
-        String middleName = scanner.nextLine().trim();
-        formatting.setMiddleName(middleName);
-
-        while (lastName.isEmpty()) {
-            System.out.println("please enter your Last name");
-            System.out.print("Last name: ");
-            lastName = scanner.nextLine().trim();
-
-
-            if (lastName.isEmpty()) {
-                System.out.println("must have a Last name");
-            }
-        }
-        formatting.setLastName(lastName);
-
-        System.out.print("Suffix: ");
-        String suffix = scanner.nextLine().trim();
-        formatting.setSuffix(suffix);
-
-        System.out.print("prefix: ");
-        String prefix = scanner.nextLine().trim();
-        formatting.setPrefix(prefix);
-
-        System.out.println(formatting);
     }
 }
-
